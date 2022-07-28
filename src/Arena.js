6fbc49034dfba988237ec2d8f421053b78533629
Otire82 <a.jeanne01@sncf.fr>
2022-07-28 11:41:14 +0200

    poo heracles ok

--- src/Arena.js
+++ src/Arena.js
@@ -1,7 +1,16 @@
 class Arena {
   constructor(size, monsters, hero) {
-    this.monsters = monsters;
     this.hero = hero;
+    this.monsters = monsters;
     this.size = size;
   }
+
+  getDistance(fighter1, fighter2) {
+    return Math.sqrt(Math.pow(fighter2.x - fighter1.x, 2) + Math.pow(fighter2.y - fighter1.y, 2)).toFixed(2)
+  };
+
+  isTouchable(attacker, defender) {
+    return this.getDistance(attacker, defender) <= attacker.getRange()
+  }
+ 
 }
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,21 +1,20 @@
 const MAX_LIFE = 100
 
 class Fighter {
-  constructor(name, strength, dexterity, image, x, y) {
+  constructor(name, strength, dexterity, image, x = 0 , y = 0, range = 1) {
     this.name = name;
     this.strength = strength;
     this.dexterity = dexterity;
     this.life = MAX_LIFE;
     this.image = image;
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
+    this.range = range
   }
 
 
-  /**
-   * Launch a strike
-   * @param Fighter defender
-   */
+  // Launch the fight
+  // Lance un combat
   fight(defender) {
     let attackPoints = this.getRandomInt(this.getDamage());
     let damages = Math.max(attackPoints - this.getDefense(defender), 0);
@@ -23,42 +22,32 @@ class Fighter {
   }
 
 
-  /**
-   * Calculate the value of the attack
-   * @returns
-   */
+  // Calculate the total attack capacity of the fighter
+  // Calcule la capacité d'attaque totale du combattant
   getDamage() {
-    return this.weapon ?
-      this.strength + this.weapon.damage :
-      this.strength;
+    return this.strength
   }
 
 
-  /**
-   * Calculate the value of the defense
-   * @returns
-   */
+  // Calculate the total defense capacity of the fighter
+  // Calcule la capacité de défense totale du combattant
   getDefense() {
-    return this.shield ?
-      this.dexterity + this.shield.protection :
-      this.dexterity;
+    return this.dexterity
   }
 
-  /**
-   * Make a random number between 1 and 100
-   * @param Number max
-   * @returns Number
-   */
+  // Generate a random value between 1 and max
+  // Génère une valeur aléatoire entre 1 et max
   getRandomInt(max) {
     return 1 + Math.floor(Math.random() * max);
   }
 
+  getRange() {
+    return this.range
+  }
 
-  /**
-  * Check if the fighters is still alive
-  * @returns Boolean
-  */
+  // Allow to know if a fighter is still alive
+  // Permet de déterminer si un combattant est encore en vie
   isAlive() {
-    return this.life > 0;
+    return this.life > 0
   }
 }
